06660c2410b386691ea33c30cdf6dfddb81554f5
Adonay <adonaypalacios98@gmail.com>
2021-03-22 00:31:28 -0600

    Crud con Plantilla AdminLTE y Datatable integrada

--- resources/views/usuarios/form.blade.php
+++ resources/views/usuarios/form.blade.php
@@ -1,22 +1,32 @@
 <h1>{{$modo}} usuario</h1>
-<label for="Nombre"> Nombre </label>
-<input type="text" name="Nombre" value="{{ isset($usuario->Nombre)?$usuario->Nombre:''}}" id="Nombre">
+<div class="card-body">
+    <div class="form-group">
+        <label for="Nombre"> Nombre </label>
+        <input type="text" class="form-control" placeholder="Ingrese el nombre" name="Nombre" value="{{ isset($usuario->Nombre)?$usuario->Nombre:''}}" id="Nombre">
+    </div>
+    <div class="form-group">
+        <label for="Apellidos"> Apellidos </label>
+        <input type="text" class="form-control" placeholder="Ingrese apellidos" name="Apellidos" value="{{ isset($usuario->Apellidos)?$usuario->Apellidos:''}}" id="Apellidos">
+    </div>
+    <div class="form-group">
+        <label for="Email"> Email </label>
+        <input type="text" class="form-control" placeholder="Ingrese email" name="Email" value="{{ isset($usuario->Email)?$usuario->Email:''}}" id="Email">
+    </div>
+    <div class="form-group">
+        <label for="Clave"> Clave </label>
+        <input type="password" class="form-control" placeholder="Clave" name="Clave" value="{{ isset($usuario->Clave)?$usuario->Clave:''}}" id="Clave">
+    </div>
+    <div class="form-group">
+        <label for="Foto"> Foto:   </label>
+        @if(isset($usuario->Foto))
+        <img src="{{asset('storage').'/'.$usuario->Foto}}" width="100" alt="">
+        @endif
+        <input type="file" name="Foto" value="" id="Foto">
+    </div>
 
-<label for="Apellidos"> Apellidos </label>
-<input type="text" name="Apellidos" value="{{ isset($usuario->Apellidos)?$usuario->Apellidos:''}}" id="Apellidos">
+    <div class="card-footer">
+        <input type="submit" class="btn btn-primary" value="{{$modo}} datos">
+        <a href="{{url('usuarios/')}}" class="btn btn-secondary">Regresar</a>
+    </div>
 
-<label for="Email"> Email </label>
-<input type="text" name="Email" value="{{ isset($usuario->Email)?$usuario->Email:''}}" id="Email">
-
-<label for="Clave"> Clave </label>
-<input type="password" name="Clave" value="{{ isset($usuario->Clave)?$usuario->Clave:''}}" id="Clave">
-
-<label for="Foto"> Foto </label>
-@if(isset($usuario->Foto))
-<img src="{{asset('storage').'/'.$usuario->Foto}}" width="100" alt="">
-@endif
-<input type="file" name="Foto" value="" id="Foto">
-
-<input type="submit" value="{{$modo}} datos">
-
-<a href="{{url('usuarios/')}}">Regresar</a>
+</div>
--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -2,46 +2,69 @@
 {{Session::get('mensaje')}}
 @endif
 
-<a href="{{url('usuarios/create')}}">Nuevo Usuario</a>
-<table class="table table-light">
-    <thead class="thead-light">
-        <tr>
-            <th>#</th>
-            <th>Foto</th>
-            <th>Nombre</th>
-            <th>Apellidos</th>
-            <th>Email</th>
-            <th>Acciones</th>
-        </tr>
-    </thead>
-    <tbody>
-        @Foreach($usuarios as $usuario)
-        <tr>
-            <td>{{$usuario->id}}</td>
-            <td>
-                <img src="{{ asset('storage').'/'.$usuario->Foto}}" width="100" alt="">
-            </td>
-            <td>{{$usuario->Nombre}}</td>
-            <td>{{$usuario->Apellidos}}</td>
-            <td>{{$usuario->Email}}</td>
-            
-            <td>
+@include('usuarios.cabecera')
 
-            <a href="{{ url('/usuarios/'.$usuario->id.'/edit') }}">
-                Editar
-            </a>
+        <!-- Main row -->
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                    <h5 class="card-title">Usuarios registrados</h5>
+                    <div class="row">
+                        <div class="col-md-6"><a href="{{url('usuarios/create')}}" class="btn btn-success">Nuevo Usuario</a></div>
+                    </div>
                 
-            
-            | 
+                        <!-- <table class="table table-light"> -->
+                            <table id="usuarios" class="table table-striped table-bordered" style="width:100%">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th>#</th>
+                                    <th>Foto</th>
+                                    <th>Nombre</th>
+                                    <th>Apellidos</th>
+                                    <th>Email</th>
+                                    <th>Acciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @Foreach($usuarios as $usuario)
+                                <tr>
+                                    <td>{{$usuario->id}}</td>
+                                    <td>
+                                        <img src="{{ asset('storage').'/'.$usuario->Foto}}" width="100" alt="">
+                                    </td>
+                                    <td>{{$usuario->Nombre}}</td>
+                                    <td>{{$usuario->Apellidos}}</td>
+                                    <td>{{$usuario->Email}}</td>
+                                    
+                                    <td>
 
-            <form action="{{ url('/usuarios/'.$usuario->id) }}" method="post">
-                @csrf 
-                {{ method_field('DELETE') }}
-                <input type="submit" onclick="return confirm('¿Seguro de eliminar el registro?')"
-                value="Borrar">
-            </form>
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <a href="{{ url('/usuarios/'.$usuario->id.'/edit') }}" class="btn btn-warning">
+                                            Editar
+                                            </a>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <form action="{{ url('/usuarios/'.$usuario->id) }}" method="post">
+                                                @csrf 
+                                                {{ method_field('DELETE') }}
+                                                <input type="submit" onclick="return confirm('¿Seguro de eliminar el registro?')"
+                                                value="Borrar" class="btn btn-danger">
+                                            </form>
+                                        </div>
+                                    </div>
+
+                                            
+                                    
+                                    
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+@include('usuarios.pie')
